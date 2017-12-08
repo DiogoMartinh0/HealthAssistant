54de9a13e181729845c16d55fd8fdf6854a3d83f
DiogoMartinho <diogom_11@hotmail.com>
2017-12-08 23:40:14 +0000

    Classes bases

---
+++ app/src/main/java/pt/isec/gps1718/g34/healthassistant/Event.java
@@ -0,0 +1,8 @@
+package pt.isec.gps1718.g34.healthassistant;
+
+
+public abstract class Event {
+    private int ID;
+
+    public int getID () { return ID; }
+}
--- app/src/main/java/pt/isec/gps1718/g34/healthassistant/MainActivity.java
+++ app/src/main/java/pt/isec/gps1718/g34/healthassistant/MainActivity.java
@@ -1,14 +1,53 @@
 package pt.isec.gps1718.g34.healthassistant;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements PrescriptionsFragment.OnFragmentInteractionListener, AppointmentsFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hello :)
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Prescriptions"));
+        tabLayout.addTab(tabLayout.newTab().setText("Appointments"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        final PagerAdapter pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
